5705c0dc05a4efe20faf667803df2d466e4cfb16
Karim Badr <karimhbadr@gmail.com>
2023-11-30 16:34:56 +0000

    updates

---
+++ Chapter 1/trial.R
@@ -0,0 +1,14 @@
+library(shiny)
+library(reactable)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server <- function(input, output, session) {
+  string <- reactive(paste0("Hello ", input$name, "!"))
+  output$greeting <- renderText(string())
+}
+
+shinyApp(ui,server)
